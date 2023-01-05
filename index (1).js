578205250b75bfa0798224735d8c756ac0e7b0fc
chandrakala <chandhu6302886523@gmail.com>
2023-01-05 08:50:01 +0530

    first commit

---
+++ index (1).js
@@ -0,0 +1,50 @@
+1.// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+let name="Masai School";
+console.log(name);
+let name1="A Transformation in Education";
+console.log(name1);
+
+
+2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+var name="chandrakala";
+console.log(name);
+var fathername="mogilappa";
+console.log(fathername);
+var mothername="lakshmidevi";
+console.log(mothername);
+
+
+3.
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+let name="chandrakala";
+let age=21;
+console.log(name,age);
+console.log(typeof(name));
+console.log(typeof(age));
+
+
+
+4.
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+console.log("******  REPORT CORD  ******");
+console.log("Name    : Chandrakala");
+console.log("School  : ZP girls high school");
+console.log("Grade   : A");
+console.log("Section : B");
+console.log("Rollno  : 7");
+console.log("Subjects: Telugu,English,Hindi");
+let Telugu=91;
+let English=80;
+let Hindi=80;
+console.log(Telugu,English,Hindi);
